5db59638ec10fa85c9eaada625009b6efdd82341
SergeyParadigm77 <paradigm77@rambler.ru>
2023-01-02 02:09:05 +0300

    Скорректирован код по задаче 5!

--- src/Main.java
+++ src/Main.java
@@ -36,7 +36,7 @@ public class Main {
 
     public static void task3() {
         short year = 2021;
-        if (year % 4 == 0 || (year >= 400 && year % 400 == 0)) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year >= 400 && year % 400 == 0)) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
